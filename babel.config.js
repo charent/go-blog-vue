2d8a29bd46c14adb4529c91a3a2f21a77b13188f
Charent <19562666+charent@users.noreply.github.com>
2021-10-14 21:26:51 +0800

    bug fixed

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,25 @@
+
+//  引入vuepress代码高亮
+const components = require('prismjs/components');
+const allLanguages = Object.keys(components.languages).filter((item) => item !== 'meta');
+
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+
+  // vuepress代码高亮
+  plugins: [
+    [
+      'prismjs',
+      {
+        // 引入全部语言
+        languages: allLanguages,
+
+        // 引入指定语言
+        // languages: ['json'],
+      },
+    ],
+  ],
 }
--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,12 @@ import '@kangc/v-md-editor/lib/style/base-editor.css';
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
 
+
 import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 
 // highlight js
-import hljs from 'highlight.js';
+// import hljs from 'highlight.js';
 import prism from 'prismjs';
 
 //配置请求数据
@@ -30,13 +31,21 @@ declare module '@vue/runtime-core' {
 }
 
 VMdPreview.use(vuepressTheme, {
-  Hljs: hljs,
+  // Hljs: hljs,
   Prism: prism,
+  extend(md) {
+    // md为 markdown-it 实例，可以在此处进行修改配置,并使用 plugin 进行语法扩展
+    // md.set(option).use(plugin);
+  },
 });
 
 VueMarkdownEditor.use(vuepressTheme, {
-  Hljs: hljs,
+  // Hljs: hljs,
   Prism: prism,
+  extend(md) {
+    // md为 markdown-it 实例，可以在此处进行修改配置,并使用 plugin 进行语法扩展
+    // md.set(option).use(plugin);
+  },
 });
 
 const app = createApp(App)
